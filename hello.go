266d9add466abc52f44082e218159c84146050b0
Zlodimir <sslupinos@gmail.com>
2014-12-04 13:53:56 +0300

    eble new gommid prvkad blead

--- hello.go
+++ hello.go
@@ -1,5 +1,6 @@
 // You can edit this code!
 // Click here and start typing.
+// Eble commentiren
 package main
 
 import "fmt"
@@ -7,3 +8,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello ебано дело!")
 }
+
+
